e41bdcf194f410db4c5bf1a8ba79160f0939b2f8
Phan Phước Ý <phanphuocy@gmail.com>
2022-04-10 17:33:03 +0700

    learned to CRUD a resource

--- leads/urls.py
+++ leads/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import index, show
+from .views import index, show, new, update, delete
 
 app_name = 'leads'
 
 urlpatterns = [
-    path('', index),
-    path('<pk>', show)
+    path('', index, name='list_index'),
+    path('new', new, name='list_new'),
+    path('<pk>', show, name='list_show'),
+    path('<pk>/update', update, name='list_update'),
+    path('<pk>/delete', delete, name='list_delete')
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Lead
+
+from .forms import LeadModelForm
+from .models import Lead, Agent
 
 
 def index(request):
@@ -17,3 +19,44 @@ def show(request, pk):
         'lead': lead
     }
     return render(request, 'leads/show.html', context=ctx)
+
+
+def new(request):
+    form = LeadModelForm()
+    if request.method == 'POST':
+        form = LeadModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/leads')
+
+    ctx = {
+        'form': form
+    }
+    return render(request, 'leads/new.html', context=ctx)
+
+
+def update(request, pk):
+    lead = Lead.objects.get(id=pk)
+    form = LeadModelForm(instance=lead)
+    print (request.method)
+    if request.method == "POST":
+        form = LeadModelForm(request.POST, instance=lead)
+        print(form)
+        print(form.is_valid())
+        if form.is_valid():
+            form.save()
+            return redirect("/leads")
+        else:
+            return HttpResponse('Form is invalid')
+
+    ctx = {
+        'form': form,
+        'lead': lead
+    }
+    return render(request, 'leads/update.html', context=ctx)
+
+
+def delete(request, pk):
+    lead = Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")
